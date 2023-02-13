aacf18709afad222e6a56a0fa1cc187878859b62
Halit Oturk <halithamza0920@gmail.com>
2023-02-13 13:55:02 +0300

    finished

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "cydeo/step_definitions",
    //     plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "junit:target/cucumber.xml"}
